4a25051db849fe1d2ca0268725fda21635a06bd2
WebDevRoxy <62933373+WebDevRoxy@users.noreply.github.com>
2022-01-03 22:04:03 +1300

    More Rows and Columns on Board
    Made it so that there are five rows and columns on the TicTacToe board instead of three.

--- GameMain.java
+++ GameMain.java
@@ -6,8 +6,8 @@ import javax.swing.*;
 public class GameMain extends JPanel implements MouseListener{
 	//Constants for game 
 	// number of ROWS by COLS cell constants 
-	public static final int ROWS = 3;     
-	public static final int COLS = 3;  
+	public static final int ROWS = 5;     
+	public static final int COLS = 5;  
 	public static final String TITLE = "Tic Tac Toe";
 
 	//constants for dimensions used for drawing
--- GameState.java
+++ GameState.java
@@ -2,7 +2,6 @@
  * Enumeration for the game states
  */
 
-
 public enum GameState {
     Playing, Draw, Cross_won, Nought_won
 }
